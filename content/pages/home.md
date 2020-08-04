05ddb4146fe86c87b426ecc65b58107a640783e2
Kevin Goldsmith <kevin@unitcircle.com>
2020-08-03 22:14:22 -0700

    Added pelican

---
+++ content/pages/home.md
@@ -0,0 +1,17 @@
+Title: Welcome to My Site
+URL:
+save_as: index.html
+
+Thank you for visiting. Welcome!
+
+# Consulting
+Leveraging almost 30 years of experience in the technology industry at early-stage startups, growth companies, and fortune 50 companies, we help leaders and leadership teams with:
+
+- Agile/Lean Product Development
+- Scaling Agile Organizations
+- Organizational Design
+- Startup advising
+
+I have consulted with leadership teams in small and mid-sized companies in the United States, the United Kingdom and Europe.
+# Speaking
+I am a sought-after international keynote speaker who has informed, entertained and inspired thousands of leaders and developers at conferences and companies around the world. I have spoken at companies such as The BBC, AO.com, ThinkMoney, Embriq, Philips, de Bijenkorf, ING, UK Department for Digital, Culture, Media and Sport, Automattic, Wooga, Bonnier, and Compare the Market.
---
+++ pelicanconf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Kevin Goldsmith'
+SITENAME = 'Nimble Autonomy'
+SITEURL = 'https://nimbleautonomy.com'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# set new root
+BLOG_ROOT_PATH = 'articles/'
+
+ARTICLE_PATHS = ['articles']
+ARTICLE_URL = BLOG_ROOT_PATH + '{slug}.html'
+ARTICLE_SAVE_AS = BLOG_ROOT_PATH + '{slug}.html'
+
+AUTHOR_SAVE_AS = ''
+AUTHOR_URL = ''
+AUTHORS_SAVE_AS = ''
+AUTHORS_URL = ''
+DAY_ARCHIVE_SAVE_AS = ''
+ARCHIVES_SAVE_AS = BLOG_ROOT_PATH + 'archives.html'
+ARCHIVES_URL = BLOG_ROOT_PATH + 'archives.html'
+CATEGORIES_SAVE_AS = BLOG_ROOT_PATH + 'categories.html'
+CATEGORIES_URL = BLOG_ROOT_PATH + 'categories.html'
+TAGS_SAVE_AS = BLOG_ROOT_PATH + 'tags.html'
+TAGS_URL = BLOG_ROOT_PATH + 'tags.html'
+CATEGORY_SAVE_AS = BLOG_ROOT_PATH + 'category/{slug}.html'
+CATEGORY_URL = BLOG_ROOT_PATH + 'category/{slug}.html'
